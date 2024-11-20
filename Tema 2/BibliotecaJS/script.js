0003dbeb76a6b22fd82c3b084d814d18191e3496
ianmiguelsoler <ianmiguel@gmail.com>
2024-11-20 14:23:39 +0100

    hecho la practica con creacion de objetos

--- Tema 2/BibliotecaJS/script.js
+++ Tema 2/BibliotecaJS/script.js
@@ -22,4 +22,28 @@ const comprobarMayorImc = (imc1,imc2) =>{
  }
 };
 
-export {unirValores, calcularIMC, comprobarMayorImc};
+
+//!----------------- Ejercicio 1 practica 2.03 -----------------------
+
+var nombreDeCurso = ["2Daw", "1Daw", "2Dam", "1Dam", "Bachillerato 2", "Bachillerato 1"];
+var año = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010];
+const nombres = ["Pedro", "Yuyu", "Miguel", "ian", "Cuyita", "Mai", "Joaquin", "Maria", "Patry"];
+
+
+const nombreAleatorios = () => {
+    var arrayCurso = []
+    let numero = Math.floor(Math.random() * 5)
+    for (let contador = 0; contador < numero; contador++) {
+        numero = Math.floor(Math.random() * 9)
+        let nombre = nombres[numero];
+        arrayCurso = [...arrayCurso, nombre];
+    }
+    if(arrayCurso.length === 0){
+        nombreAleatorios()
+    }
+    else{
+        return arrayCurso
+    }
+}
+
+export {unirValores, calcularIMC, comprobarMayorImc, nombreAleatorios};
---
+++ Tema 2/Practica Constructor/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+import {nombreAleatorios} from "../BibliotecaJS/script.js";
+
+//! Ejercicio 1
+//? Variables
+
+document.addEventListener("DOMContentLoaded", () => {
+    const nombreDeCurso = ["2Daw", "1Daw", "2Dam", "1Dam", "Bachillerato 2", "Bachillerato 1"];
+    const año = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010];
+    const nombres = ["Pedro", "Yuyu", "Miguel", "ian", "Cuyita", "Mai", "Joaquin", "Maria", "Patry"];
+
+var nombreDeCursoAleatorio = nombreDeCurso[4];
+var añoCurso = año[5];
+var descripcion = "Hola esto es una descripción";
+var alumnado = nombreAleatorios();
+if (alumnado === undefined){
+    alumnado = nombreAleatorios();
+}
+
+const constructorCurso = (nombreDeCursoAleatorio, añoCurso, descripcion, alumnado) =>{
+
+    var curso ={
+        "Nombre Curso": nombreDeCursoAleatorio,
+        "Año Del Curso": añoCurso,
+        "Descripcion": descripcion,
+        "Alumnado": {alumnado},
+    }
+    return curso
+}
+
+
+var curso = constructorCurso(nombreDeCursoAleatorio, añoCurso, descripcion, alumnado);
+
+const mostrarFuncionObjeto = (curso) => {
+    for (const clave in curso) {
+        if (Object.prototype.hasOwnProperty.call(curso, clave)) {
+            const element = curso[clave];
+            if(clave === "Alumnado"){
+                console.log("Se Mostrará el interior de alumnado a continuación:")
+                mostrarFuncionObjeto(element)
+            }
+            else{
+                 console.log(`${clave} => ${element}`)
+            }
+           
+        }
+    }
+}
+
+mostrarFuncionObjeto(curso)
+
+},false)
+
+
